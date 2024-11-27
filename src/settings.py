6cf598229c3b20cac977c8bdd8f82575bc0f3039
Tim Hanewich <tahanewich@live.com>
2024-11-27 16:44:03 -0500

    Try 4, GPIO #

--- src/settings.py
+++ src/settings.py
@@ -4,4 +4,4 @@ pin_drive1:int = 13 # the first drive pin of the L293D
 pin_drive2:int = 15 # the second drive pin of the L293D
 
 # SG-90 Servo for steering (not GPIO's, pin #'s)
-pin_steering:int = 16
+pin_steering:int = 4
