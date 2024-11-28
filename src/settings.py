d10d891390928c832e375f298d9ee6fd2b5a0a56
Tim Hanewich <tahanewich@live.com>
2024-11-28 08:33:09 -0500

    Swap pins

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 # TT motor driver pins (not GPIO's, pin #'s)
 pin_safety:int = 11 # the "safety" input of the L293D (top left) 
-pin_drive1:int = 13 # the first drive pin of the L293D
-pin_drive2:int = 15 # the second drive pin of the L293D
+pin_drive1:int = 15 # the first drive pin of the L293D
+pin_drive2:int = 13 # the second drive pin of the L293D
 
 # SG-90 Servo for steering
 # use GPIO # here, not pin #, because pigpio, what we'll be using to run the steering, using the GPIO #
