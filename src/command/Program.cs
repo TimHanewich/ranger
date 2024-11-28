bec734624a86006d577dfdf799e599a78dc0e37f
Tim Hanewich <tahanewich@live.com>
2024-11-28 15:27:13 -0500

    Added ranger command

---
+++ src/command/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Spectre.Console;
+
+namespace RangerCommand
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("What is your name? > ");
+                string? input = Console.ReadLine();
+                if (input != null)
+                {
+                    Console.WriteLine("Hello, " + input + "!");
+                }
+                else
+                {
+                    Console.WriteLine("Hey, give me your name!");
+                }
+            }
+        }
+    }
+}
