3555c1627b5b87331e1cb97fa13575bf057ce27f
Tim Hanewich <tahanewich@live.com>
2024-11-28 11:16:59 -0500

    Started package creation

--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-print("Hello!")
+import psutil
+
+def prepare() -> dict:
+    """Prepares a return packet"""
+
+    ToReturn:dict = {}
+
+    # get memory info
+    meminfo = psutil.virtual_memory()
+    ToReturn["memp"] = round(meminfo.percent / 100, 3) # memory used, as a percent of total memory
+    ToReturn["memu"] = meminfo.used # memory used, in bytes
+    ToReturn["mema"] = meminfo.available # memory available, in bytes
+
+    # get disk info
+    du = psutil.disk_usage("/")
+    ToReturn["diska"] = du.free
+
+    # get network io information
+    netio = psutil.net_io_counters()
+    ToReturn["bsent"] = netio.bytes_sent
+    ToReturn["brecv"] = netio.bytes_recv
+
+    # return it
+    return ToReturn
+
+
+d = prepare()
+print(d)
