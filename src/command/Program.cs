60c3e5a125190b965ebb839daa1fd8170b9065b9
Tim Hanewich <tahanewich@live.com>
2024-11-28 15:41:59 -0500

    Started framework for command program

--- src/command/Program.cs
+++ src/command/Program.cs
@@ -12,7 +12,51 @@ namespace RangerCommand
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(GetAzureConnectionString());
+            MainAsync().Wait();
+        }
+
+        public static async Task MainAsync()
+        {
+            //Ask what to do
+            SelectionPrompt<string> sp = new SelectionPrompt<string>();
+            sp.Title("What do you want to do?");
+            sp.AddChoice("Monitor, receive, and show incoming messages in the queue");
+            sp.AddChoice("Send commands to Ranger");
+            string selected = AnsiConsole.Prompt<string>(sp);
+
+            //Handle what to do
+            if (selected == "Monitor, receive, and show incoming messages in the queue")
+            {
+                
+                //Authenticate to Azure Queue Storage
+                QueueClient qc = new QueueClient(GetAzureConnectionString(), "c2r");
+                AnsiConsole.Markup("Checking if queue '[blue]" + "c2r" + "[/]' exists... ");
+                if (await qc.ExistsAsync())
+                {
+                    AnsiConsole.MarkupLine("[green]it exists![/]");
+                }
+                else
+                {
+                    AnsiConsole.MarkupLine("[yellow]it does not exist![/]");
+                    AnsiConsole.Markup("Creating queue '[blue]c2r[/]'... ");
+                    await qc.ExistsAsync();
+                    AnsiConsole.MarkupLine("[green]created[/]!");
+                }
+
+                //Clear messages
+                AnsiConsole.Markup("Clearing messages in queue... ");
+                await qc.ClearMessagesAsync();
+                AnsiConsole.MarkupLine("[green]cleared![/]");
+                
+            }
+            else if (selected == "Send commands to Ranger")
+            {
+                Console.WriteLine("Not done yet.");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, I do not know how to handle that!");
+            }
         }
 
 
