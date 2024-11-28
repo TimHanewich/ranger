229ba1abb76bbf5759aaae31f041fc8b538ef6d1
Tim Hanewich <tahanewich@live.com>
2024-11-28 15:33:59 -0500

    Function to get azure connection string

--- src/command/Program.cs
+++ src/command/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Spectre.Console;
+using Azure;
+using Azure.Storage;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System.Threading.Tasks;
 
 namespace RangerCommand
 {
@@ -7,19 +12,21 @@ namespace RangerCommand
     {
         public static void Main(string[] args)
         {
-            while (true)
+            Console.WriteLine(GetAzureConnectionString());
+        }
+
+
+
+
+        private static string GetAzureConnectionString()
+        {
+            string PathToTxtFile = Path.Combine(Directory.GetCurrentDirectory(), "..", "azure_connection_string.txt");
+            if (System.IO.File.Exists(PathToTxtFile) == false)
             {
-                Console.Write("What is your name? > ");
-                string? input = Console.ReadLine();
-                if (input != null)
-                {
-                    Console.WriteLine("Hello, " + input + "!");
-                }
-                else
-                {
-                    Console.WriteLine("Hey, give me your name!");
-                }
+                throw new Exception("Unable to get Azure Storage Connection String! azure_connection_string.txt does not exist!");
             }
+            string ToReturn = System.IO.File.ReadAllText(PathToTxtFile);
+            return ToReturn;
         }
     }
 }
