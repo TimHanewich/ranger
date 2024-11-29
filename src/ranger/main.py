f8e3bd9ec1898d1f88e2d3359a6cc581d89a8061
Tim Hanewich <tahanewich@live.com>
2024-11-28 22:31:11 -0500

    Added demo script

---
+++ src/ranger/main.py
@@ -0,0 +1,28 @@
+print("Hello, I'm Ranger! Now loading...")
+import vision
+import settings
+import comms
+import time
+
+print("Entering infinite loop!")
+while True:
+
+    # set up payload
+    print("Preparing payload with standard inclusions...")
+    payload:dict = comms.prepare() # prepares with standard inclusions
+
+    # capture image?
+    if settings.include_image:
+        print("Capturing image... ")
+        b64:str = vision.capture()
+        imgdict:dict = {"b64": b64, "width": 160, "height": 120}
+        payload["image"] = imgdict
+
+    # send
+    print("Sending payload...")
+    comms.send(payload)
+
+    # wait period of time
+    for i in range(settings.pulse_frequency_seconds):
+        print("Sending next pulse in " + str(settings.pulse_frequency_seconds - i) + "... ")
+        time.sleep(1.0)
