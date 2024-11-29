456f5cfb3f704f34d3c0fd945c34c429f040662f
Tim Hanewich <tahanewich@live.com>
2024-11-29 09:42:46 -0500

    New processing function

--- src/command/Program.cs
+++ src/command/Program.cs
@@ -91,60 +91,8 @@ namespace RangerCommand
                         //If it was JSON, work with it. If not, show an error
                         if (msg != null) //It was JSON! Work with it!
                         {
-                            
-                            //Is there an "image" property?
-                            JProperty? prop_image = msg.Property("image");
-                            if (prop_image != null)
-                            {
-                                AnsiConsole.Markup("Reconstructing image... ");
-
-                                JObject image = (JObject)prop_image.Value;
-
-                                //Get base64
-                                string base64 = "";
-                                JProperty? prop_base64 = image.Property("base64");
-                                if (prop_base64 != null)
-                                {
-                                    base64 = prop_base64.Value.ToString();
-                                }
-
-                                //Get width
-                                int width = 0;
-                                JProperty? prop_width = image.Property("width");
-                                if (prop_width != null)
-                                {
-                                    width = Convert.ToInt32(prop_width.Value.ToString());
-                                }
-
-                                //Get height
-                                int height = 0;
-                                JProperty? prop_height = image.Property("height");
-                                if (prop_height != null)
-                                {
-                                    height = Convert.ToInt32(prop_height.Value.ToString());
-                                }
-
-                                //Reconstruct
-                                byte[] bytes = Convert.FromBase64String(base64);
-                                Bitmap bm = new Bitmap(width, height);
-                                int OnPixel = 0;
-                                for (int y = 0; y < height; y++)
-                                {
-                                    for (int x = 0; x < width; x++)
-                                    {
-                                        System.Drawing.Color c = System.Drawing.Color.FromArgb(255, bytes[OnPixel], bytes[OnPixel], bytes[OnPixel]);
-                                        bm.SetPixel(x, y, c);
-                                        OnPixel = OnPixel + 1;
-                                    }
-                                }
-
-                                //Save the image
-                                bm.Save(@"C:\Users\timh\Downloads\tah\ranger\image.jpg");
-
-                                //Print that there was an image
-                                AnsiConsole.MarkupLine("[green]image unpacked![/]");
-                            }
-
+                            //System.IO.File.WriteAllText(@"C:\Users\timh\Downloads\tah\ranger\message.json", msg.ToString(Formatting.Indented));
+                            ProcessReceivedMessage(msg);
                         }
                         else //It did not parse into JSON correctly.
                         {
@@ -164,25 +112,7 @@ namespace RangerCommand
             }
             else if (selected == "TEST")
             {
-                //Read base64 and convert
-                string b64 = System.IO.File.ReadAllText(@"C:\Users\timh\Downloads\tah\ranger\b64.txt");
-                byte[] bytes = Convert.FromBase64String(b64);
-
-                //Reconstruct
-                Bitmap bm = new Bitmap(160, 120);
-                int OnPixel = 0;
-                for (int y = 0; y < 120; y++)
-                {
-                    for (int x = 0; x < 160; x++)
-                    {
-                        System.Drawing.Color c = System.Drawing.Color.FromArgb(255, bytes[OnPixel], bytes[OnPixel], bytes[OnPixel]);
-                        bm.SetPixel(x, y, c);
-                        OnPixel = OnPixel + 1;
-                    }
-                }
-
-                //Save
-                bm.Save(@"C:\Users\timh\Downloads\tah\ranger\image.jpg");
+                ProcessReceivedMessage(JObject.Parse(System.IO.File.ReadAllText(@"C:\Users\timh\Downloads\tah\ranger\message.json")));
             }
             else
             {
@@ -203,5 +133,138 @@ namespace RangerCommand
             string ToReturn = System.IO.File.ReadAllText(PathToTxtFile);
             return ToReturn;
         }
+
+        private static string ConvertBytesToHumanReadable(long bytes)
+        {
+            if (bytes < 1024)
+            {
+                return bytes.ToString("#,##0") + " B";
+            }
+            else if (bytes >= 1024 && bytes < 1048576)
+            {
+                float kb = Convert.ToSingle(bytes) / 1024f;
+                return kb.ToString("#,##0.0") + " KB";
+            }
+            else if (bytes >= 1048576 && bytes < 1073741824)
+            {
+                float mb = Convert.ToSingle(bytes) / 1048576f;
+                return mb.ToString("#,##0.0") + " MB";
+            }
+            else
+            {
+                float gb = Convert.ToSingle(bytes) / 1073741824f;
+                return gb.ToString("#,##0.0") + " GB";
+            }
+        }
+
+        //Print the details of a received message into the console, unpacks the attached image, etc.
+        private static void ProcessReceivedMessage(JObject msg)
+        {
+            //Example standard payload: {'memp': 0.269, 'memu': 69828608, 'mema': 327864320, 'diska': 228248584192, 'bsent': 544165, 'brecv': 197279}
+
+            //Get memory percent (memp)
+            JProperty? prop_memp = msg.Property("memp");
+            if (prop_memp != null)
+            {
+                float memp = Convert.ToSingle(prop_memp.Value.ToString());
+                AnsiConsole.MarkupLine("Memory Used %: [blue][bold]" + memp.ToString("#0.0%") + "[/][/]");
+            }
+
+            //Get memory used (memu)
+            JProperty? prop_memu = msg.Property("memu");
+            if (prop_memu != null)
+            {
+                long memu = Convert.ToInt64(prop_memu.Value.ToString());
+                AnsiConsole.MarkupLine("Memory Used: [blue][bold]" + ConvertBytesToHumanReadable(memu) + "[/][/]");
+            }
+
+            //Get memory available
+            JProperty? prop_mema = msg.Property("mema");
+            if (prop_mema != null)
+            {
+                long mema = Convert.ToInt64(prop_mema.Value.ToString());
+                AnsiConsole.MarkupLine("Memory Available: [blue][bold]" + ConvertBytesToHumanReadable(mema) + "[/][/]");
+            }
+
+            //Get disk available
+            JProperty? prop_diska = msg.Property("diska");
+            if (prop_diska != null)
+            {
+                long diska = Convert.ToInt64(prop_diska.Value.ToString());
+                AnsiConsole.MarkupLine("Disk Available: [blue][bold]" + ConvertBytesToHumanReadable(diska) + "[/][/]");
+            }
+
+            //bytes sent
+            JProperty? prop_bsent = msg.Property("bsent");
+            if (prop_bsent != null)
+            {
+                long bsent = Convert.ToInt64(prop_bsent.Value.ToString());
+                AnsiConsole.MarkupLine("Sent: [blue][bold]" + ConvertBytesToHumanReadable(bsent) + "[/][/]");
+            }
+
+            //Bytes received
+            JProperty? prop_brecv = msg.Property("brecv");
+            if (prop_brecv != null)
+            {
+                int brecv = Convert.ToInt32(prop_brecv.Value.ToString());
+                AnsiConsole.MarkupLine("Received: [blue][bold]" + ConvertBytesToHumanReadable(brecv) + "[/][/]");
+            }
+
+            //Is there an "image" property?
+            JProperty? prop_image = msg.Property("image");
+            if (prop_image != null)
+            {
+                AnsiConsole.Markup("Reconstructing image... ");
+
+                JObject image = (JObject)prop_image.Value;
+
+                //Get base64
+                string base64 = "";
+                JProperty? prop_base64 = image.Property("base64");
+                if (prop_base64 != null)
+                {
+                    base64 = prop_base64.Value.ToString();
+                }
+
+                //Get width
+                int width = 0;
+                JProperty? prop_width = image.Property("width");
+                if (prop_width != null)
+                {
+                    width = Convert.ToInt32(prop_width.Value.ToString());
+                }
+
+                //Get height
+                int height = 0;
+                JProperty? prop_height = image.Property("height");
+                if (prop_height != null)
+                {
+                    height = Convert.ToInt32(prop_height.Value.ToString());
+                }
+
+                //Reconstruct
+                byte[] bytes = Convert.FromBase64String(base64);
+                Bitmap bm = new Bitmap(width, height);
+                int OnPixel = 0;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        System.Drawing.Color c = System.Drawing.Color.FromArgb(255, bytes[OnPixel], bytes[OnPixel], bytes[OnPixel]);
+                        bm.SetPixel(x, y, c);
+                        OnPixel = OnPixel + 1;
+                    }
+                }
+
+                //Save the image
+                bm.Save(@"C:\Users\timh\Downloads\tah\ranger\image.jpg");
+
+                //Print that there was an image
+                AnsiConsole.MarkupLine("[green][bold]image unpacked![/][/]");
+            }
+
+        }
+
+
     }
 }
