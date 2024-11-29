e21a0a7d27f252e6f0a23c580798c8bc45550722
Tim Hanewich <tahanewich@live.com>
2024-11-28 22:28:28 -0500

    Removed capture1

--- src/ranger/vision.py
+++ src/ranger/vision.py
@@ -4,15 +4,7 @@ import settings
 from PIL import Image
 import base64
 
-def capture() -> bytes:
-    """Makes a command line command to capture an image using fswebcam and returns the file content as bytes"""
-    subprocess.run(settings.capture_command, shell=True, capture_output=True, text=True) # run the command
-    with open("./img.jpg", "rb") as file:
-        file_content = file.read() # save the file content into memory
-    os.remove("./img.jpg") # delete the file
-    return file_content
-
-def capture2() -> str:
+def capture() -> str:
     """Makes a command line command to capture an image using fswebcam, converts it to grayscale, and returns the bytes of the image as base64"""
 
     # run command
