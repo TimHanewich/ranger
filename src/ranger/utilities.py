c1be5f95a7473285a7d513ccc6eb8ca8453de4f0
Tim Hanewich <tahanewich@live.com>
2024-11-30 11:45:08 -0500

    Added webcam_connected check

--- src/ranger/utilities.py
+++ src/ranger/utilities.py
@@ -8,3 +8,9 @@ def pigpiod_running() -> bool:
 def start_pigpiod() -> None:
     """Attempts to start the `pigpiod` daemon by running `sudo pigpiod`."""
     subprocess.run(["sudo", "pigpiod"])
+
+def webcam_connected() -> None:
+    """Trys to determine if a USB webcam is connected, simply be seeing if any USB is connected"""
+    output:bytes = subprocess.run(["lsusb"], capture_output=True).stdout
+    outputs:str = output.decode()
+    return outputs.count("\n") >= 2 # if there are 2 or more lines, a USB must be connected, so we'll just assume it is a webcam. if not, it is not connected
