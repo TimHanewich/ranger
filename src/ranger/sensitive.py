d5c7f6ac28f7e776edcc16917c9455f9111fe72b
Tim Hanewich <tahanewich@live.com>
2024-11-30 08:03:28 -0500

    Added queue url

--- src/ranger/sensitive.py
+++ src/ranger/sensitive.py
@@ -1,5 +1,5 @@
 # put any keys/secrets here
 
-azure_queue_url_send:str = "" # the queue that ranger should use to transmit messages to command ("r2c"), i.e. "https://rangercomms.queue.core.windows.net/r2c"
-azure_queue_url_recv:str = "" # the queue that ranger should use to receive messages from command ("c2r"), i.e. "https://rangercomms.queue.core.windows.net/c2r"
+azure_queue_url_send:str = "https://rangercomms.queue.core.windows.net/r2c" # the queue that ranger should use to transmit messages to command ("r2c"), i.e. "https://rangercomms.queue.core.windows.net/r2c"
+azure_queue_url_recv:str = "https://rangercomms.queue.core.windows.net/c2r" # the queue that ranger should use to receive messages from command ("c2r"), i.e. "https://rangercomms.queue.core.windows.net/c2r"
 azure_queue_sas:str = "" # SAS token to the azure storage account with sufficient privileges
