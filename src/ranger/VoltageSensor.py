cc149abdf29db9fe51c10d0798989859a2bb8eaf
Tim Hanewich <tahanewich@live.com>
2024-12-02 09:48:14 -0500

    Check if open

--- src/ranger/VoltageSensor.py
+++ src/ranger/VoltageSensor.py
@@ -7,12 +7,15 @@ class VoltageSensor:
 
     def _read_raw(self) -> int:
         """Reads the raw integer value coming through on the UART"""
-        self.ser.reset_input_buffer() # clear the RX buffer so that way we know what we are about to read is brand new data
-        next:bytes = self.ser.readline()
-        nexts:str = next.decode()
-        nexts = nexts.replace("\r\n", "")
-        nexti:int = int(nexts) # convert
-        return nexti    
+        if self.ser.is_open():
+            self.ser.reset_input_buffer() # clear the RX buffer so that way we know what we are about to read is brand new data
+            next:bytes = self.ser.readline()
+            nexts:str = next.decode()
+            nexts = nexts.replace("\r\n", "")
+            nexti:int = int(nexts) # convert
+            return nexti    
+        else:
+            raise Exception("Cannot read voltage because the serial connection was closed!")
     
     def close(self) -> None:
         """Closes the serial connection."""
