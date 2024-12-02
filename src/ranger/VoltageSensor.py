9663ebc315b22fd2a1570815f0ef82b0991d319b
Tim Hanewich <tahanewich@live.com>
2024-12-02 09:42:40 -0500

    Added voltage sensor

---
+++ src/ranger/VoltageSensor.py
@@ -0,0 +1,14 @@
+import serial
+
+class VoltageSensor:
+
+    def __init__(self, bus:str = "/dev/serial0", baudrate:int = 9600):
+        self.ser:serial.Serial = serial.Serial(bus, baudrate)
+
+    def _read_raw(self) -> int:
+        """Reads the raw integer value coming through on the UART"""
+        next:bytes = self.ser.readline()
+        nexts:str = next.decode()
+        nexts = nexts.replace("\r\n", "")
+        nexti:int = int(nexts) # convert
+        return nexti    
