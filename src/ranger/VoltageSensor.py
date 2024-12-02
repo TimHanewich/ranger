8c429ec83109a160db843604e4cbb48acf7c6e8f
Tim Hanewich <tahanewich@live.com>
2024-12-02 09:49:00 -0500

    Use bool

--- src/ranger/VoltageSensor.py
+++ src/ranger/VoltageSensor.py
@@ -7,7 +7,7 @@ class VoltageSensor:
 
     def _read_raw(self) -> int:
         """Reads the raw integer value coming through on the UART"""
-        if self.ser.is_open():
+        if self.ser.is_open:
             self.ser.reset_input_buffer() # clear the RX buffer so that way we know what we are about to read is brand new data
             next:bytes = self.ser.readline()
             nexts:str = next.decode()
