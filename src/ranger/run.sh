8a45cef76e47943d8f48eca85ee1d64258eab2ab
Tim Hanewich <tahanewich@live.com>
2024-12-07 08:45:59 -0500

    Added run.sh script

---
+++ src/ranger/run.sh
@@ -0,0 +1,5 @@
+# activate the virtual environment
+source "/home/tim/rvenv/bin/activate" # replace with the path to your virtual environment's activate script
+
+# run the python program
+python3 "/home/tim/ranger/src/ranger/main.py" # replace with wherever you have Ranger's "main.py" file
