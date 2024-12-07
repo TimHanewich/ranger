4cf836d0dd9e1c4cd16b1d2f93b54b2c19fba37b
Tim Hanewich <tahanewich@live.com>
2024-12-07 08:50:22 -0500

    More comments and echos

--- src/ranger/run.sh
+++ src/ranger/run.sh
@@ -1,5 +1,12 @@
+# This is Ranger's run script
+# This simple bash script takes care of the activation of the virtual environment and firing of the python program
+# All done in one step!
+# Configure this script to run on startup if you want the ranger program to be activated as soon as it is powered up.
+
 # activate the virtual environment
+echo "Ranger Run: Activating virtual environment..."
 source "/home/tim/rvenv/bin/activate" # replace with the path to your virtual environment's activate script
 
 # run the python program
+echo "Ranger Run: Entering Python program..."
 python3 "/home/tim/ranger/src/ranger/main.py" # replace with wherever you have Ranger's "main.py" file
